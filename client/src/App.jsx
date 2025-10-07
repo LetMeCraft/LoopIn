fa1d3e36fd107a2da0a722017795a3afc36a1392
LetMeCraft <meetarpit.codes@gmail.com>
2025-10-07 14:17:11 +0530

    setting pages/routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Messages from './assets/pages/Messages'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+   <>
+   <Routes>
+     <Route path='/' element={<Login />}>
+     <Route index element={<Feed/>}/>
+     <Route path='messages' element={<Messages/>}/>
+     <Route path='messages/:userId' element={<ChatBox/>}/>
+     <Route path='connections' element={<Connections/>}/>
+     <Route path='discover' element={<Discover/>}/>
+     <Route path='profile' element={<Profile/>}/>
+     <Route path='profile/:profileId' element={<Profile/>}/>
+     <Route path='create-post' element={<CreatePost/>}/>
+     </Route>
+   </Routes>
+   </>
   )
 }
 
